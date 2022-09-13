989e36df43462ffde4dbaf1dbe10d71c09203b9b
CheolHyun Mun <zooxop@gmail.com>
2022-09-13 20:29:41 +0900

    add events for Textview height

--- TodoTalk/TodoTalk/TalkVC.swift
+++ TodoTalk/TodoTalk/TalkVC.swift
@@ -1,16 +1,31 @@
 import UIKit
 
+
+// 1. 채팅 table view 터치하면 키보드 내려가도록 처리.
+// 2. send 누르면 채팅 되도록 기능 추가.
+// 3. bottom area color 세팅
+
 class TalkVC: UIViewController {
 
-    @IBOutlet weak var talkTableView: UITableView! {
+    @IBOutlet weak var contentsTableView: UITableView! {
         didSet {
-            talkTableView.delegate = self
-            talkTableView.dataSource = self
-            talkTableView.separatorStyle = .none
+            contentsTableView.delegate = self
+            contentsTableView.dataSource = self
+            contentsTableView.separatorStyle = .none
             
-            talkTableView.register(UINib(nibName: "MyTalkCell", bundle: nil), forCellReuseIdentifier: "MyTalkCell")
+            contentsTableView.register(UINib(nibName: "MyTalkCell", bundle: nil), forCellReuseIdentifier: "MyTalkCell")
+            contentsTableView.keyboardDismissMode = .interactive
         }
     }
+    
+    @IBOutlet weak var inputTextView: UITextView! {
+        didSet {
+            inputTextView.delegate = self
+//            self.setTextViewHeight(inputTextView)
+        }
+    }
+    
+    @IBOutlet weak var inputViewBottomMargin: NSLayoutConstraint!
     
     var talk: TodoTalk!
     
@@ -21,6 +36,53 @@ class TalkVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 키보드 관련 옵저버 - 상태를 알려줌
+        // - 키보드가 올라올 때
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        // - 키보드가 내려갈 때
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        // default height를 고정값으로 처리할 수 없음. (기기마다 폰트 사이즈가 다를 수 있으므로.)
+        // -> 한 글자 미리 입력해놓고, textview 높이 조절 이벤트를 호출.
+        // 이벤트가 끝나면 다시 text를 클리어 해준다.
+        self.inputTextView.text = "."
+        self.textViewDidChange(self.inputTextView)
+        self.inputTextView.text = ""
+    }
+    
+    @objc func keyboardWillShow(noti: Notification) {
+        // 키보드의 frame 가져오기
+        let notiInfo = noti.userInfo!
+        let keyboardFrame = notiInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        
+        // 키보드 frame의 높이 - safe area 높이
+        let height = keyboardFrame.size.height - self.view.safeAreaInsets.bottom
+        
+        // 키보드가 올라올/내려올 때 호출되는 애니메이션의 동작시간 가져오기.
+        let animationDuration = notiInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+        
+        // Animation 적용
+        UIView.animate(withDuration: animationDuration) {
+            // 높이 적용해주기
+            self.inputViewBottomMargin.constant = height
+            
+            // layoutIfNeeded => layout 변경에 대한 이벤트를 큐의 뒤쪽이 아닌 앞으로 가져오고
+            // 그에 대해 즉시 실행해주는 명령.
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillHide(noti: Notification){
+        // 키보드의 frame 가져오기
+        let notiInfo = noti.userInfo!
+        
+        // 키보드가 올라올/내려올 때 호출되는 애니메이션의 동작시간 가져오기.
+        let animationDuration = notiInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.inputViewBottomMargin.constant = 0
+            self.view.layoutIfNeeded()
+        }
     }
     
     @IBAction func saveTalkContent(_ sender: Any) {
@@ -46,4 +108,21 @@ extension TalkVC: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+}
+
+extension TalkVC: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        let size = CGSize(width: view.frame.width, height: .infinity)
+        let estimatedSize = textView.sizeThatFits(size)
+        
+        textView.constraints.forEach { constraint in
+            if estimatedSize.height <= 30  || estimatedSize.height >= 100 {
+                
+            } else {
+                if constraint.firstAttribute == .height {
+                    constraint.constant = estimatedSize.height
+                }
+            }
+        }
+    }
 }
