cea8e3d86a7ec10e43ff18e2b60ea954f5929f0f
CheolHyun Mun <zooxop@gmail.com>
2022-10-03 21:16:59 +0900

    - scrolling naturally between of keyboard and tableview

--- TodoTalk/TodoTalk/TalkVC.swift
+++ TodoTalk/TodoTalk/TalkVC.swift
@@ -37,20 +37,32 @@ class TalkVC: UIViewController {
     @IBOutlet weak var inputViewBottomMargin: NSLayoutConstraint!
     
     var talk: TodoTalk!
+    var talkContents: [TalkContents]!
     
-    lazy var talkContents: [TalkContents]! = {
+    func loadTalkContents() {
         let contents = self.talk.joinTalkId?.array as! [TalkContents]
-        return contents.reversed()
-    }()
+        talkContents = contents.reversed()
+    }
+    
+    func scrollToBottom(animated: Bool) {
+        let lastIndexPath = IndexPath(row: (self.talkContents.count - 1), section: 0)
+        if lastIndexPath[1] >= 0 {
+            DispatchQueue.main.async {
+                self.contentsTableView.scrollToRow(at: lastIndexPath, at: UITableView.ScrollPosition.bottom, animated: animated)
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.loadTalkContents()
         // 키보드 관련 옵저버 - 상태를 알려줌
         // - 키보드가 올라올 때
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         // - 키보드가 내려갈 때
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        // - 키보드가 완전히 올라온 뒤
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
         
         // default height를 고정값으로 처리할 수 없음. (기기마다 폰트 사이즈가 다를 수 있으므로.)
         // -> 한 글자 미리 입력해놓고, textview 높이 조절 이벤트를 호출.
@@ -62,6 +74,19 @@ class TalkVC: UIViewController {
         // 제스쳐 핸들러 등록
         // 화면 전체를 UIViewController로 덮어놨기 때문에, touchesBegan 만으로는 동작하지 않는다.
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)))
+        
+        self.scrollToBottom(animated: false)
+        
+        self.contentsTableView.bottomAnchor.constraint(equalTo: self.view.keyboardLayoutGuide.topAnchor).isActive = true
+
+    }
+    
+    // Observer 해제
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidShowNotification, object: nil)
     }
     
     // 화면 터치하면 키보드 내려가도록.
@@ -69,22 +94,28 @@ class TalkVC: UIViewController {
         self.view.endEditing(true)
     }
     
+    @objc func keyboardDidShow(noti: Notification) {
+        self.scrollToBottom(animated: true)
+    }
+    
     @objc func keyboardWillShow(noti: Notification) {
         // 키보드의 frame 가져오기
         let notiInfo = noti.userInfo!
         let keyboardFrame = notiInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
         
+        // let keyboardFrame = notiInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
         // 키보드 frame의 높이 - safe area 높이
         let height = keyboardFrame.size.height - self.view.safeAreaInsets.bottom
         
         // 키보드가 올라올/내려올 때 호출되는 애니메이션의 동작시간 가져오기.
         let animationDuration = notiInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
-        
+
         // Animation 적용
         UIView.animate(withDuration: animationDuration) {
             // 높이 적용해주기
             self.inputViewBottomMargin.constant = 0 - height
-            
+            // self.contentsTableView.
+
             // layoutIfNeeded => layout 변경에 대한 이벤트를 큐의 뒤쪽이 아닌 앞으로 가져오고
             // 그에 대해 즉시 실행해주는 명령.
             self.view.layoutIfNeeded()
@@ -92,24 +123,35 @@ class TalkVC: UIViewController {
     }
     
     @objc func keyboardWillHide(noti: Notification){
+       
         // 키보드의 frame 가져오기
         let notiInfo = noti.userInfo!
-        
+
         // 키보드가 올라올/내려올 때 호출되는 애니메이션의 동작시간 가져오기.
         let animationDuration = notiInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
-        
+
         UIView.animate(withDuration: animationDuration) {
             self.inputViewBottomMargin.constant = 0
             self.view.layoutIfNeeded()
         }
+ 
     }
     
     // 전송 버튼 이벤트
     @IBAction func saveTalkContent(_ sender: Any) {
+        
         if let inputText = self.inputTextView.text {
             CoredataManager.shared.insertTalkContents(todoTalk: self.talk, content: inputText)
-            self.inputTextView.text = ""
+            self.loadTalkContents()
+            self.inputTextView.text.removeAll()
+            
             self.textViewDidChange(self.inputTextView)
+            
+            let lastIndexPath = IndexPath(row: (self.talkContents.count - 1), section: 0)
+
+            contentsTableView.insertRows(at: [lastIndexPath], with: .none)
+            contentsTableView.scrollToRow(at: lastIndexPath, at: UITableView.ScrollPosition.bottom, animated: true)
+            
         }
     }
 }
