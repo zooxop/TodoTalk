37a224d26647da6da552fb06770fdb26efe5b4ff
CheolHyun Mun <zooxop@gmail.com>
2022-10-28 14:25:03 +0900

    - Launch Screen 추가 - App Icon 추가

--- TodoTalk/TodoTalk/AppDelegate.swift
+++ TodoTalk/TodoTalk/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        sleep(1) // LaunchScreen 대기 시간 1초
         return true
     }
 
